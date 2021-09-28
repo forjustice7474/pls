484d7faf8a77b05842cf3027e1b94efdd89a62f3
forjustice <forjustice7474@gmail.com>
2021-09-28 13:19:58 +0900

    09281319

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -8,4 +8,4 @@ rm vibe_data.zip
 cd ..
 mv data/vibe_data/vibe_data/sample_video.mp4 .
 mkdir -p $HOME/.torch/models/
-mv data/vibe_data/yolov3.weights $HOME/.torch/models/
+mv data/vibe_data/vibe_data/yolov3.weights $HOME/.torch/models/
