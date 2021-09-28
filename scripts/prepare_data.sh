6596c7403e9ec60ff4411bfcd26adbe1b8cdb164
forjustice <forjustice7474@gmail.com>
2021-09-28 13:12:10 +0900

    09281312

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -6,6 +6,6 @@ gdown "https://drive.google.com/uc?export=download&id=1UMlItQvgRUCp14af3U2_kLV6L
 unzip vibe_data.zip
 rm vibe_data.zip
 cd ..
-mv data/vibe_data/sample_video.mp4 .
+mv data/vibe_data/vibe_data/sample_video.mp4 .
 mkdir -p $HOME/.torch/models/
 mv data/vibe_data/yolov3.weights $HOME/.torch/models/
