fcf07e33becfbc5c2d1e6140fe2c323ebe522fbe
forjustice <forjustice7474@gmail.com>
2021-09-28 12:55:18 +0900

    colab

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -2,7 +2,7 @@
 
 mkdir -p data
 cd data
-gdown "https://drive.google.com/uc?export=download&id=18JdST6dZKpqp2TUdvlp6eJrbMCGYr0Y9"
+gdown "https://drive.google.com/uc?export=download&id=1UMlItQvgRUCp14af3U2_kLV6LhYFPlw_"
 unzip vibe_data.zip
 rm vibe_data.zip
 cd ..
